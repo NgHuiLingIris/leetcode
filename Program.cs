ccd1068d7a419e91f6a2b099e1f6cec153ca0de0
NgHuiLingIris <49146877+NgHuiLingIris@users.noreply.github.com>
2019-05-07 17:21:32 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace MinIncrementUniqueArray
+{
+    public class Solution
+    {
+        public static void Main()
+        {
+            int[] A = new int[] { 165, 489, 594, 646, 69, 455 };
+            int increment = MinIncrementForUnique(A);
+            Console.WriteLine(increment);
+        }
+        public static int MinIncrementForUnique(int[] A)
+        {
+            int increment = 0;
+            do
+            {
+                for (int i = 0; i < A.Length - 1; i++)
+                {
+                    if (A[i] == A[i + 1])
+                    {
+                        A[i + 1] = A[i + 1] + 1;
+                        increment++;
+                    }
+                }
+                Array.Sort(A);
+            } while (checkUnique(A) == false);
+            return increment;
+        }
+        public static bool checkUnique(int[] A)
+        {
+            if (A.Length != A.Distinct().Count())
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
